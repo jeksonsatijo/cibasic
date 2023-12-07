c3fa2f2148261522dbfaa9155ba7e57d6d723f67
jeksonsatijo <jeksonsatijo@gmail.com>
2023-12-07 15:14:07 +0700

    update welcome page

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -7,6 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="/favicon.ico">
 
+    // add feature basic ci
+
     <!-- STYLES -->
 
     <style {csp-style-nonce}>
